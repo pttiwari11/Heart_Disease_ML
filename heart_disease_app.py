d12ae5c1e3225af44b54f6b75b82661eb70a9f98
Pushpendra Tiwari <78895037+pttiwari11@users.noreply.github.com>
2022-07-21 23:09:28 -0700

    Update heart_disease_app.py

--- heart_disease_app.py
+++ heart_disease_app.py
@@ -3,7 +3,7 @@
 import numpy as np
 import pickle
 import os
-from collection import Mapping
+from collections.abc import Mapping
 from flask import Flask, request, render_template
 
 # Load ML model
